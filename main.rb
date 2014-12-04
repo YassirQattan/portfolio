6ae8280d7f386cb20b8a05041759b167f25a827c
Y.Q.1993@hotmail.com <Yassir Qattan>
2014-12-04 10:14:33 -0500

    Portfolio

--- main.rb
+++ main.rb
@@ -2,22 +2,22 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get "/" do
-    @title = "Tim Regan-Porter's Portfolio"
-    @description = "This site showcases all of the awesome things that Tim Regan-Porter has done."
+    @title = "Yassir Qattan's Portfolio"
+    @description = "This site showcases all of the awesome things that Yassir Qattan has done."
     @home = "active"
     erb :home
 end
 
 get '/about' do
     @title = "About Me"
-    @description = "This page provides a short bio for Tim Regan-Porter"
+    @description = "This page provides a short bio for Yassir Qattan"
     @about = "active"
     erb :about
 end
 
 get '/works' do
     @title = "My Clips"
-    @description = "This page provides links to Tim Regan-Porter's published clips"
+    @description = "This page provides links to Yassir Qattan's published clips"
     @works = "active"
     erb :work
 end
